49230a6c9a1a9282c7064b23d3a8f1293111af59
ConfusionOrb221 <nate1898@gmail.com>
2019-11-21 21:24:00 -0500

    added the styled components library
    Added styled components and used that to style my cards

--- src/cards/Cards.js
+++ src/cards/Cards.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import "../App.css";
+import styled from 'styled-components';
 //date, explanation, media_type, service_version, title, url
 // { date, explanation, media_type, service_version, title, url }
 let apikey = '8YWiTnmfl6Ih93s7DQjPhefibdfwbU2olKDeSq4v';
@@ -8,6 +9,7 @@ const date = new Date();
 const Cards = () =>  {
     const [apiInfo, setApiInfo] = useState();
     const [loading, setLoading] = useState(true);
+
     // eslint-disable-next-line no-extend-native
     function getDays(){
         const d = new Date(date.getFullYear(), date.getMonth()+1, 0);
@@ -29,6 +31,23 @@ const Cards = () =>  {
     } 
     console.log(apiInfo);
 
+    const Photo = styled.img`
+        width: 400px;
+        height: 600px;
+    `;
+    const FloatingText = styled.div`
+        position: absolute;
+        ${props => (props.type === 'top-middle' ? 
+            `top: 10%;
+             left: 50%;
+             transform: translate(-50%, -50%);` : null
+        )}
+        ${props => (props.type === 'bottom-right' ?
+            `bottom: 3%;
+             right: 10%;` : null
+        )}
+    `;
+
     return (
         <div className='picture-container'>
             {loading ? <div className='loading'>...loading </div> : 
@@ -36,12 +55,12 @@ const Cards = () =>  {
             <div className="card-image">
                 { i.media_type === 'image' ? 
                 <>
-                    <img src={i.url} alt={i.title}></img> 
-                    <div className="top-middle"> {i.title} </div>
-                    <div className="bottom-right"> {i.date} </div>
+                    <Photo src={i.url} alt={i.title}></Photo> 
+                    <FloatingText type='top-middle'> {i.title} </FloatingText>
+                    <FloatingText type='bottom-right'> {i.date} </FloatingText>
                 </>
                 : 
-                <iframe width="560" height="315" title={i.title} src={i.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+                <iframe width="100%" height="100%" title={i.title} src={i.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
                 }
             </div>))}
         </div>
